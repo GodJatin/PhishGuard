1653cfa2663fef8aa91e7e380d570677802c73fe
GodJatin <224jatin2006@gmail.com>
2026-05-22 07:33:54 +0530

    refactor: revert to minimal valid middleware and default next config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,21 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config, { nextRuntime, webpack }) => {
-    console.log("--- Webpack Build Runtime:", nextRuntime);
-    if (nextRuntime === 'edge') {
-      config.node = {
-        ...config.node,
-        __dirname: 'mock',
-      };
-      config.plugins.push(
-        new webpack.DefinePlugin({
-          __dirname: JSON.stringify('/'),
-        })
-      );
-    }
-    return config;
-  },
+  /* config options here */
 };
 
 export default nextConfig;
